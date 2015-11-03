f89fec6de3c0b3b9aa7af6bc55c58d1cb3a9a513
gr4viton <lordmutty@gmail.com>
2015-11-03 22:32:18 +0100

    beep on difference - not functional

--- main.py
+++ main.py
@@ -9,28 +9,49 @@
 import time
 import datetime
 import urllib.request
+import win32api
+from itertools import chain
 
 def set_online_time():
     url = 'http://just-the-time.appspot.com/'
     print('Getting current online time from [', url, ']')
-    str_tim = str( urllib.request.urlopen(url).read() )
-    print('Got time [',str_tim,']')
-    _win_set_time( time.strptime(str_tim, "b'%Y-%m-%d %H:%M:%S '") )
+    string_time_online = str( urllib.request.urlopen(url).read() )
+    print('Got time [', string_time_online, ']')
+    _win_set_time( time.strptime(string_time_online, "b'%Y-%m-%d %H:%M:%S '") )
 
-def _win_set_time(struct_time):
-    print('struct_time = "',struct_time,'"')
-    import win32api
+def _win_set_time(struct_time_online):
+    print('struct_time_online [', struct_time_online, ']')
     # http://timgolden.me.uk/pywin32-docs/win32api__SetSystemTime_meth.html
     # win32api.SetSystemTime(year, month , dayOfWeek , day , hour , minute , second , millseconds )
-    win32api.SetSystemTime(struct_time.tm_year,
-                          struct_time.tm_mon,
-                          struct_time.tm_wday,
-                          struct_time.tm_mday,
-                          struct_time.tm_hour,
-                          struct_time.tm_min,
-                          struct_time.tm_sec,
+    beep_on_difference(struct_time_online)
+    win32api.SetSystemTime(struct_time_online.tm_year,
+                          struct_time_online.tm_mon,
+                          struct_time_online.tm_wday,
+                          struct_time_online.tm_mday,
+                          struct_time_online.tm_hour,
+                          struct_time_online.tm_min,
+                          struct_time_online.tm_sec,
                           0
                           )
+    
+def beep_on_difference(struct_time_online, beep_seconds = 5.0):
 
+
+    # beep pc speaker if the time is off more than X seconds
+    tuple_time_system = win32api.GetSystemTime()
+    tuple_time_system = tuple(chain(*(tuple_time_system[0:2], tuple_time_system[3:])))
+    print('tuple_time_system [', tuple_time_system, ']')
+    # print(sum(tuple_time_system))
+    struct_time_system = datetime.datetime(*tuple_time_system)
+    # struct_time_system = time.mktime(tuple_time_system)
+    # struct_time_system = time.mktime(
+    print('struct_time_system [', struct_time_system, ']')
+
+    print(type(struct_time_system), type(struct_time_online))
+    difference = datetime.timedelta(struct_time_online, struct_time_system)
+    print('system to online difference [', difference, ']')
+    if(difference > time.second(beep_seconds)):
+        print
+        print('\7') # functional only when runned through command line ($ python.exe main.py)
 if __name__ == '__main__':
     set_online_time()
